552ddd4e4863ec2d8ccc0576c2dc73cca8059e1f
arpitg94 <arpitgahlot94@gmail.com>
2024-08-14 13:24:56 +0530

    Some autogenerated files added

--- iosApp/iosApp/Models/Post.swift
+++ iosApp/iosApp/Models/Post.swift
@@ -1,1 +1,18 @@
 
+import Foundation
+import CoreData
+
+@objc(Post)
+public class Post: NSManagedObject {
+
+    @NSManaged public var userId: Int64
+    @NSManaged public var title: String?
+    @NSManaged public var content: String?
+
+}
+
+extension Post {
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Post> {
+        return NSFetchRequest<Post>(entityName: "Post")
+    }
+}
--- iosApp/iosApp/Models/User.swift
+++ iosApp/iosApp/Models/User.swift
@@ -1,1 +1,17 @@
 
+import Foundation
+import CoreData
+
+@objc(User)
+public class User: NSManagedObject {
+
+    @NSManaged public var name: String?
+    @NSManaged public var email: String?
+
+}
+
+extension User {
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<User> {
+        return NSFetchRequest<User>(entityName: "User")
+    }
+}
