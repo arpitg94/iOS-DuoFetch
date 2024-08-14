ecb2d61652dd6b9dd44371a7f2e7fa1d42f3c64a
arpitg94 <arpitgahlot94@gmail.com>
2024-08-14 13:04:01 +0530

    Barebones Synchronization daemon

---
+++ iosApp/iosApp/SynchronizationDaemon.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+class SynchronizationDaemon {
+    static let shared = SynchronizationDaemon()
+
+    func synchronizeWithServer() {
+        
+
+    func performSyncOnRequest() {
+        synchronizeWithServer()
+    }
+}
